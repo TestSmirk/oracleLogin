76b5f616dd554e913857059423ff96c920f75488
TestSmirk <d.kc@qq.com>
2019-10-28 14:22:51 +0800

    Fixes #3

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 oracle 自动登陆
 
 
+
+
 * [安装node](https://nodejs.org/zh-cn/download/package-manager/)
 * clone 到本地 or server, `git clone https://github.com/TestSmirk/oracleLogin.git`
 * `npm install` or `yarn add` 要下载 Chromium带宽不高到国内机器可能会慢
@@ -14,3 +16,13 @@ const Password = "";
 * `node oracle.js` 运行
 
 * crontab -e  add 每周执行 `0 0 * * 0 /path/to/node /path/to/oracle.js` 由于要多次启动和关闭Chromium可能会有僵尸进程占用cpu内存需要定时清理 `*/10 * * * * /usr/bin/killall -9 chrome`
+
+
+---
+
+加入登录成功判断  
+
+* 失败:  
+![](https://github.com/TestSmirk/oracleLogin/blob/master/imgs/TIM截图20191028140608.png)
+* 成功:  
+![](https://github.com/TestSmirk/oracleLogin/blob/master/imgs/TIM截图20191028141014.png)
--- oracle.js
+++ oracle.js
@@ -1,39 +1,45 @@
 const puppeteer = require('puppeteer');
 
-const loginUrl = "https://console.ap-seoul-1.oraclecloud.com";
+const loginUrl = "https://console.us-ashburn-1.oraclecloud.com/";
 const Tenant = "";
 const UserName = "";
 const Password = "";
 
 (async () => {
-    if (Tenant === ""||UserName===""||Password===""||loginUrl==="") {
+    if (Tenant === "" || UserName === "" || Password === "" || loginUrl === "") {
         console.log("请输入");
         process.exit(0);
         return
     }
-    const browser = await puppeteer.launch({headless :true ,args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
 
-    console.log("等待页面加载 "+loginUrl);
+    console.log("等待页面加载 " + loginUrl);
     await page.goto(loginUrl);
     await page.waitFor("#tenant");
     console.log("输入tenant");
-    await page.type("#tenant",Tenant);
+    await page.type("#tenant", Tenant);
     var inputTypeSubmit = 'input[type="submit"]';
     console.log("点击确定");
     await page.click(inputTypeSubmit);
     console.log("等待下一页加载");
     await page.waitFor("#username");
     console.log("输入用户名");
-    await page.type("#username",UserName);
+    await page.type("#username", UserName);
     console.log("输入密码");
-    await page.type("#password",Password);
+    await page.type("#password", Password);
     // await page.click(inputTypeSubmit);
 
-    console.log("点击登陆");
-    await page.click("body > div.main-container > div.right-panel > table > tbody > tr:nth-child(3) > td:nth-child(3) > form > div:nth-child(14) > input[type=submit]");
-    console.log("success"+new Date());
-    await page.close();
+    console.log("点击登录");
+    page.on("response", function (resp) {
+        if (resp.url().indexOf("authorize") > -1) {
+            console.log(resp.ok() ? "登录成功" : "登录失败");
+            page.close();
 
-    process.exit(0);
+            process.exit(0);
+        }
+    });
+    await page.click("body > div.main-container > div.right-panel > table > tbody > tr:nth-child(3) > td:nth-child(3) > form > div:nth-child(14) > input[type=submit]");
+
+
 })();
